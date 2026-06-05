367c5c6ca8b17aacde3aa9425e24079f7512ca93
Mateus Maia <maiamateusotavio@gmail.com>
2026-06-05 12:46:50 -0300

    feat: implement the three calculators

---
+++ Smartwyre.DeveloperTest/Services/Calculators/FixedRateRebateCalculator.cs
@@ -0,0 +1,20 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Services.Calculators;
+
+public class FixedRateRebateCalculator : IRebateCalculator
+{
+    public bool CanHandle(IncentiveType incentiveType)
+        => incentiveType == IncentiveType.FixedRateRebate;
+
+    public decimal? Calculate(Rebate rebate, Product product, CalculateRebateRequest request)
+    {
+        if (!product.SupportedIncentives.HasFlag(SupportedIncentiveType.FixedRateRebate))
+            return null;
+
+        if (rebate.Percentage == 0 || product.Price == 0 || request.Volume == 0)
+            return null;
+
+        return product.Price * rebate.Percentage * request.Volume;
+    }
+}
